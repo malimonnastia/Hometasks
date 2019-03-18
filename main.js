778237d68417d0a8f8b3823f8861f80e155a4c2a
malimonnastia <47988758+malimonnastia@users.noreply.github.com>
2019-03-18 22:46:50 +0200

    lesson15

---
+++ main.js
@@ -0,0 +1,44 @@
+var myString1='Hello World!';
+    num=42;
+    bool=true;
+    array=[0,5,10,15];
+    object={name:'the Object'};
+    N = new Number(1800);
+    i=20;
+    digit =300;
+    myString2 ='Subjects to ecstatic children he. Could ye leave up as built match. Dejection agreeable attention set suspected led offending.'
+
+
+console.log(myString1);
+console.log(num);
+console.log(bool);
+console.log(array);
+console.log(object);
+console.log(N.toFixed(2));
+console.log(++i);
+console.log(i++);
+console.log(digit +=40);
+console.log(digit -=50);
+console.log(digit *=60);
+console.log(digit /=30);
+console.log(digit %=42);
+console.log(Math.PI);
+console.log(Math.round(89.279));
+console.log(Math.random()*10);
+console.log(Math.pow(2,5));
+console.log(myString2.length);
+console.log(myString2.indexOf('leave up'));
+console.log(myString2.replace('children','adults'));
+console.log(myString2.toUpperCase());
+console.log(myString2.toLowerCase());
+
+
+
+
+
+
+
+
+
+
+
